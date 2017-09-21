a7f9cc72f9112e16a56679bdb3cb99fc9f18c609
gene-yoo <gene.y.yoo@gmail.com>
2017-09-21 12:17:48 -0400

    Add class definition and methods for School

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,26 @@
-# code here!
+class School
+  attr_accessor :roster
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @roster = {}
+  end
+
+  def add_student(student_name, grade)
+    self.roster[grade] = [] if !(self.roster[grade])
+    self.roster[grade] << student_name
+  end
+
+  def grade(grade_no)
+    self.roster[grade_no]
+  end
+
+  def sort
+    new_roster = {}
+    self.roster.each do |grade, students|
+      new_roster[grade] = students.sort
+    end
+    self.roster = new_roster
+  end
+end
